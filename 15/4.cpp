c687b5fc9f0e829e99413c2901d9617b185b6cac
lqz-tmp <2867222731@qq.com>
2018-07-07 08:52:05 +0800

    2018-7-7 8:51 Update
    2018-7-7 8:51 Update

--- 15/4.cpp
+++ 15/4.cpp
@@ -4,11 +4,11 @@ int a[805],b[30];
 int main(){
 	int n;
 	cin>>n;
-	for (int i=1;i<=int(sqrt(800));++i){
+	for (int i=1;i*i<=n;++i){
 		b[i]=i*i;
 	}
 	a[0]=1;
-	for (int i=1;i<=int(sqrt(800));++i){
+	for (int i=1;i*i<=n;++i){
 		for (int j=1;j<=n;++j){
 			if(j>=b[i]){
 				a[j]+=a[j-b[i]];
@@ -18,3 +18,13 @@ int main(){
 	cout<<a[n]<<endl;
 	return 0;
 }
+
+/*
+for (int i=1;i*i<=n;++i){
+	for (int j=1;j<=n;++j){
+		if(j>=b[i]){
+			a[i][j]=a[i-1][j]+a[i][j-b[i]];
+		}
+	}
+}
+*/
