84d5257c7f7aa08b01d2c48f2d7731833f1ed712
lqz-tmp <2867222731@qq.com>
2018-07-07 19:08:52 +0800

    2018-7-7 19:08 Update
    2018-7-7 19:08 Update

--- 16/7.cpp
+++ 16/7.cpp
@@ -6,7 +6,7 @@ bool b[2005][2005]{false}; //是否遍历过
 int c[2005][2005];
 int m,n;
 
-void bl(int x,int y){
+void bl(int x,int y){      //递归遍历
     b[x][y]=true;
     if (a[x-1][y]=='.'&&!b[x-1][y]) bl(x-1,y);
     if (a[x][y-1]=='.'&&!b[x][y-1]) bl(x,y-1);
@@ -15,7 +15,7 @@ void bl(int x,int y){
     return;
 }
 
-void check(int x,int y){
+void check(int x,int y){   //炸死的僵尸数
     for (int i=1;x-i>=1;++i){
         if (a[x-i][y]=='G') c[x][y]++;
         if (a[x-i][y]=='#') break;
@@ -41,19 +41,19 @@ int main(){
     string ts;
     for (int i=1;i<=n;++i){
         cin>>ts;
-        for (int j=0;j<=ts.length()-1;++j){
+        for (int j=0;j<=(int)ts.length()-1;++j){
             a[i][j+1]=ts[j];
         }
     }
     bl(x,y);
     for (int i=1;i<=n;++i){
-        for (int j=1;j<=n;++j){
-            if (a[i][j]=='.') check(i,j);
+        for (int j=1;j<=m;++j){
+            if (b[i][j]) check(i,j);
         }
     }
     int ml=0;
     for (int i=1;i<=n;++i){
-        for (int j=1;j<=n;++j){
+        for (int j=1;j<=m;++j){
             if (b[i][j]&&c[i][j]>ml) ml=c[i][j];
         }
     }
